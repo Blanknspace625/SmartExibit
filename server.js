0cbbd89e2ea4d75492f9f09327559f52e1ef9683
root <root@ip-172-31-0-30.ap-southeast-1.compute.internal>
2020-09-20 14:26:51 +0000

    https support added (support Chrome loads website remotely)

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const fs = require('fs');
 
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 80;
-app.listen(port);
+const privKey = fs.readFileSync('/etc/letsencrypt/live/epf.johnnybread.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/epf.johnnybread.com/fullchain.pem', 'utf8');
+const credentials = {
+  key: privKey, cert: certificate
+};
 
+var app = express();
 
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
 
 //register api routes
 var routes = require('./routes');
 routes(app);
 
+var httpServ = http.createServer(app);
+var httpsServ = https.createServer(credentials, app);
 
-console.log('Server started on: Port ' + port);
+httpServ.listen(80, "::");
+httpsServ.listen(443, "::");
+console.log('HTTP & HTTPS server started!');
